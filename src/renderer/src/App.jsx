ef08ef5d8219f179c25a0b4e10076628c4e226a3
Patryk Kurdziel <patryk.kurdziel@protonmail.com>
2024-10-05 18:04:26 +0200

    chore: clean up initialization and configure necessary dependencies

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,35 +1,15 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { Button } from '@mantine/core';
+// import electronLogo from './assets/electron.svg'
 
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+	// const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 
-  return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
+	return (
+		<div className='bg-blue-400'>
+			<h1>Hello World</h1>
+			<Button variant='filled'>Button</Button>
+		</div>
+	);
 }
 
-export default App
-
+export default App;
