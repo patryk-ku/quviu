95a99369f747bdd2edc4eefff6de00294b5d0506
Patryk Kurdziel <patryk.kurdziel@protonmail.com>
2025-04-20 19:44:12 +0200

    feat(Settings): update theme selector UI

--- src/renderer/src/components/Tabs/Settings.jsx
+++ src/renderer/src/components/Tabs/Settings.jsx
@@ -51,19 +51,7 @@ export default function Settings({ setColors, ffmpegPaths }) {
 			<div className='mt-2 grid grid-cols-1 gap-6'>
 				<div className='grid gap-1'>
 					<Title order={5}>Theme</Title>
-					<div className='mb-1 flex items-center gap-1'>
-						<Text>Current theme:</Text>
-						<Badge size='sm' radius='md' color={colorMap[color][0]}>
-							Primary
-						</Badge>
-						<Badge variant='gradient' size='sm' radius='md'>
-							Gradient
-						</Badge>
-						<Badge size='sm' radius='md' color={colorMap[color][1]}>
-							Secondary
-						</Badge>
-					</div>
-					<div className='mb-2 flex'>
+					<div className='grid grid-cols-[auto,1fr] gap-4'>
 						<div className='app-background-alt rounded-lg border border-[--tab-border-color] px-1.5 pb-1'>
 							<ColorPicker
 								size='xs'
@@ -90,14 +78,28 @@ export default function Settings({ setColors, ffmpegPaths }) {
 								swatchesPerRow={6}
 							/>
 						</div>
-					</div>
-					<div className='flex'>
-						<Switch
-							label='Light theme'
-							radius='md'
-							checked={computedColorScheme === 'light'}
-							onChange={(event) => toggleColorScheme(event.target.checked)}
-						/>
+						<div className='grid gap-1'>
+							<div className='flex items-center gap-1'>
+								<Text>Current theme:</Text>
+								<Badge size='sm' radius='md' color={colorMap[color][0]}>
+									Primary
+								</Badge>
+								<Badge variant='gradient' size='sm' radius='md'>
+									Gradient
+								</Badge>
+								<Badge size='sm' radius='md' color={colorMap[color][1]}>
+									Secondary
+								</Badge>
+							</div>
+							<div className='flex'>
+								<Switch
+									label='Light theme'
+									radius='md'
+									checked={computedColorScheme === 'light'}
+									onChange={(event) => toggleColorScheme(event.target.checked)}
+								/>
+							</div>
+						</div>
 					</div>
 				</div>
 
